5f9ada7b5ac86a7f01713212fc7b3251ec07d41b
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-03 07:51:28 +0530

    03-april-String object comparison

---
+++ Test.java
@@ -0,0 +1,23 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		//SCP -> literals -> String Constant Pool
+		String s1 = "sunil";
+		String s3 = "sunil";
+
+		//new String -> Stores on Heap
+		String s2 = new String("sunil");		
+		String s4 = new String("sunil");		
+
+		boolean res = (s1 == s3);
+
+		boolean eq = s1.equals(s3);
+
+		System.out.println("Res:"+res);
+
+		System.out.println("Equals:"+eq);
+
+	}
+}
+
