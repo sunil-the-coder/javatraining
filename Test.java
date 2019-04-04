799d62b82356cd34bb7e9b8c88d0eada2bd39d7e
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-04 08:31:45 +0530

    equals method with null handling

--- Test.java
+++ Test.java
@@ -1,33 +1,73 @@
 class Student
 {
+	private int id;
+
+	Student(int id) {
+		this.id = id;
+	}
+
+	public boolean equals(Object obj) {
+		System.out.println("Student equals() called");
+
+		if(obj == null)
+			return false;
+
+		//1. Identify the object properties for the object which are unique.
+
+		Student s2 = (Student)obj;
+
+		if(this.id == s2.id)	
+			return true;	
+		
+		return false;
+	}
+
 	public static void main(String[] args)
 	{
-		System.out.println("student main called");
+		Student s1 = new Student(10);
+		Student s2 = new Student(20);
+
+		boolean f = s1.equals(null);
+
+		System.out.println("Result:"+f);
+
+		String str1 = new String("sunil");
+		String str2 = new String("sunil");
+
+		System.out.println(str1.equals(str2));
 	}
 }
 
+
+class A
+{
+	public void disp() {
+		System.out.println("A disp");	
+	}
+}
+
+class B extends A
+{
+	public void disp() {
+		System.out.println("B disp");	
+	}
+
+	public void show() {
+		System.out.println("B disp");	
+	}
+}
 public class Test
 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Test main called");
-		//SCP -> literals -> String Constant Pool
-		String s1 = "sunil";
-		String s3 = "sunil";
-
-		//new String -> Stores on Heap
-		String s2 = new String("sunil");		
-		String s4 = new String("sunil");		
-
-		boolean res = (s1 == s3);
-
-		boolean eq = s1.equals(s3);
-
-		System.out.println("Res:"+res);
-
-		System.out.println("Equals:"+eq);
+		A a = new B();
+		a.disp();
+		//a.show();
 
 	}
 }
+
+
+
 
