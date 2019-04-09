13746a36b9485798c24f63900e596f6fe1ee381a
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-09 07:45:58 +0530

    static members

--- first/src/com/psl/example/client/Test.java
+++ first/src/com/psl/example/client/Test.java
@@ -2,53 +2,42 @@ package com.psl.example.client;
 
 import java.util.Scanner;
 
-//import com.psl.example.Student;
-
 class Student {
 
 	private int id;
 	private String name;
+	private static int data;
 
 	public Student(int id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
+	public void addData() {
+		data += 2;
+	}
+
 	public void disp() {
-		System.out.println("ID:" + id + ",Name:" + name);
+		System.out.println("ID:" + id + ",Name:" + name + ",Data:" + data);
 	}
 }
 
 public class Test {
 
 	public static void main(String[] args) {
+
+		Student s1 = new Student(10,"sunil");
+		s1.addData();
+		s1.disp();
 		
+		Student s2 = new Student(11,"anil");
+		s2.addData();
+		s2.disp();
 		
-		Student s = new Student();
-		Student s = new Student();
-		Student s = new Student();
-		Student s = new Student();
+		Student s3 = new Student(12,"ganesh");
+		s3.addData();
+		s3.disp();
 		
-		
-		
-		
-		int count = dispObjectCount();
-		
-		
-		
-		
-		
-		Scanner scan = new Scanner(System.in);
-		int no = scan.nextInt();
-		String name = scan.nextLine();
-		
-		
-		//Array of objects
-		Student[] studs = new Student[5];
-		for (int i = 0; i < studs.length; i++) {
-			studs[i] = new Student(i+1,"A"+i);
-			studs[i].disp();
-		}
 		
 	}
 }
