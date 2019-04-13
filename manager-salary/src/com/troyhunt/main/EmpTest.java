8976ec9106b1dba2102c0117b0039fd32034dd57
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-13 09:49:40 +0530

    Sorting managers

--- manager-salary/src/com/troyhunt/main/EmpTest.java
+++ manager-salary/src/com/troyhunt/main/EmpTest.java
@@ -13,19 +13,36 @@ public class EmpTest {
 		managers[1] = new Manager(2, "PQR", "Science", 40000, 6000);
 		managers[2] = new Manager(3, "XYZ", "IT", 80000, 2000);
 
-		int max = managers[0].getSalary();
-		int index = -1;
-		
-		for (int i = 1; i < managers.length; i++) {
-			if(managers[i].getSalary() > max) {
-				max = managers[i].getSalary();
-				index = i;
-			}
+		sortEmployees(managers);
+
+		for (Employee employee : managers) {
+			System.out.println(employee);
 		}
-		
-		if(index != -1)
-			System.out.println(managers[index]);
-		else
-			System.out.println(managers[0]);
+
+		/*
+		 * int max = managers[0].getSalary(); int index = -1;
+		 * 
+		 * for (int i = 1; i < managers.length; i++) { if(managers[i].getSalary() > max)
+		 * { max = managers[i].getSalary(); index = i; } }
+		 * 
+		 * if(index != -1) System.out.println(managers[index]); else
+		 * System.out.println(managers[0]);
+		 */
+	}
+
+	private static void sortEmployees(Employee[] managers) {
+
+		for (int i = 0; i < managers.length; i++) {
+			for (int j = i+1; j < managers.length; j++) {
+
+				if (managers[i].getSalary() > managers[j].getSalary()) {
+					// swap
+					Employee e = managers[i];
+					managers[i] = managers[i + 1];
+					managers[i + 1] = e;
+				}
+			}
+
+		}
 	}
 }
