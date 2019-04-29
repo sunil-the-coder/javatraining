d6de3b400430d638e599b0edf7e8fd7439fe9a3f
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-29 08:32:18 +0530

    copying big files

--- file-handling/src/FileTest.java
+++ file-handling/src/FileTest.java
@@ -1,31 +1,54 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
+
+class MyResource implements AutoCloseable {
+	public void write() {
+		System.out.println("myresource write....");
+	}
+
+	@Override
+	public void close() {
+		System.out.println("myresource closed.");
+	}
+}
 
 public class FileTest {
 
 	public static void main(String[] args) {
 
+		try (MyResource resource = new MyResource();) {
+			System.out.println("Resource opened...");
+		} catch (Exception e) {
+
+		}
+
 		String fileName = "/home/sunil/demo.txt";
 //		writeToFile(fileName);
 		// readFromFile(fileName);
 
-		final String srcFile = "/home/sunil/first.sh";
-		final String destFile = "/home/sunil/second.sh";
+		final String srcFile = "/home/sunil/ubuntu-18.04.1-desktop-amd64.iso";
+		final String destFile = "/home/sunil/data/ubuntu-18.04.1-desktop-amd64.iso";
 
 		copy(srcFile, destFile);
 	}
 
 	private static void copy(String srcFile, String destFile) {
 
-		try (Reader reader = new FileReader(srcFile); 
-				Writer writer = new FileWriter(destFile);) {
-			int value;
-			while ((value = reader.read()) >= 0)
-				writer.write(value);
+		try (InputStream is = new FileInputStream(srcFile); OutputStream os = new FileOutputStream(destFile);) {
+			int totalReads;
+			System.out.println("Copying started....");
+			
+			byte[] data = new byte[2 * 1024];
+			while ((totalReads = is.read(data)) >= 0)
+				os.write(data,0,totalReads);
 
 			System.out.println("Copy Done.");
 
