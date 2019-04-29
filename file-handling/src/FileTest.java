7ad94039abf7a438c491319228277b912f66ba41
Sunil Patil <sunilnobeltraining@gmail.com>
2019-04-29 07:54:45 +0530

    try with resources

--- file-handling/src/FileTest.java
+++ file-handling/src/FileTest.java
@@ -11,7 +11,7 @@ public class FileTest {
 
 		String fileName = "/home/sunil/demo.txt";
 //		writeToFile(fileName);
-	//	readFromFile(fileName);
+		// readFromFile(fileName);
 
 		final String srcFile = "/home/sunil/first.sh";
 		final String destFile = "/home/sunil/second.sh";
@@ -21,62 +21,34 @@ public class FileTest {
 
 	private static void copy(String srcFile, String destFile) {
 
-		Reader reader = null;
-		Writer writer = null;
-
-		try {
-			reader = new FileReader(srcFile);
-			writer = new FileWriter(destFile);
-
+		try (Reader reader = new FileReader(srcFile); 
+				Writer writer = new FileWriter(destFile);) {
 			int value;
 			while ((value = reader.read()) >= 0)
 				writer.write(value);
-			
+
 			System.out.println("Copy Done.");
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (writer != null)
-					writer.close();
-				if (reader != null)
-					reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-
 	}
 
 	private static void writeToFile(String fileName) {
 
-		Writer writer = null;
-
-		try {
-			writer = new FileWriter(fileName);
+		try (Writer writer = new FileWriter(fileName)) {
 			writer.write("Welcome to file handling");
 			writer.flush();// move contents from buffer to actual hard disk
-
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (writer != null)
-					writer.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 
 	}
 
 	private static void readFromFile(String fileName) {
-		Reader reader = null;
-		try {
-			reader = new FileReader(fileName);
+		try (Reader reader = new FileReader(fileName);) {
 			int ch;
 			while ((ch = reader.read()) >= 0)
 				System.out.print((char) ch);
@@ -85,13 +57,6 @@ public class FileTest {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (reader != null)
-					reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 
 		/*
