95d8b497f2f50112b1af581256f237f2564af5ce
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-01 12:54:44 +0530

    anonymous classe

--- nested-classes/src/NestedTest.java
+++ nested-classes/src/NestedTest.java
@@ -1,8 +1,7 @@
 
 class Outer {
 	public void disp() {
-		
-		//Method local inner class
+		// Method local inner class
 		class Inner {
 			public void disp() {
 				System.out.println("method local inner disp called..");
@@ -14,9 +13,34 @@ class Outer {
 	}
 }
 
+interface A {
+	void show();
+}
+
 public class NestedTest {
 
+	public static void callApi(A ref) {
+		ref.show();
+	}
+
 	public static void main(String[] args) {
-		new Outer().disp();
+
+		A testRef = new A() {
+			public void show() {
+				System.out.println("Anonymous lambda called");
+			};			
+		};
+		
+		callApi(testRef);
+
+		A ref = new A() {
+			@Override
+			public void show() {
+				System.out.println("Anonymous called");
+			}
+		};
+
+		ref.show();
+
 	}
 }
