014747bb5b99d574648fbc7d88f54487590372a7
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-09 09:00:00 +0530

    sorting map keys using treeMap

--- collections-fundamentals/src/com/itp/training/Application.java
+++ collections-fundamentals/src/com/itp/training/Application.java
@@ -1,23 +1,23 @@
 package com.itp.training;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		Map nameMap = new HashMap();
+		Map nameMap = new TreeMap();
 
 		nameMap.put(10, "SUNIL");
-		nameMap.put(11, "ANIL");
-		nameMap.put(12, "SHIVANSH");
-		nameMap.put(13, "GANESH");
+		nameMap.put(20, "ANIL");
+		nameMap.put(19, "SHIVANSH");
+		nameMap.put(23, "GANESH");
 		nameMap.put(14, "MAULI");
 		nameMap.put(15,null);
 		nameMap.put(16,null);
-		nameMap.put(null,"AKASH");
-		nameMap.put(null,"AKSHAY");
+		//nameMap.put(null,"AKASH");
+		//nameMap.put(null,"AKSHAY");
 		nameMap.put(14, "MANDAL");
 		
 		System.out.println(nameMap);
