7631b9a449a3ae9fb9754906a2c778606f8fd596
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-09 08:27:03 +0530

    sorting strings

--- collections-fundamentals/src/com/itp/training/Application.java
+++ collections-fundamentals/src/com/itp/training/Application.java
@@ -15,10 +15,10 @@ public class Application {
 	public static void main(String[] args) {
 
 		Set c = new TreeSet();
-		c.add(10);
-		c.add(10);
-		c.add(5);
-		c.add(4);
+		c.add("SUNIL");
+		c.add("ANIL");
+		c.add("GANESH");
+		c.add("ARYAN");
 
 		System.out.println("Size:" + c.size());
 		System.out.println(c);
