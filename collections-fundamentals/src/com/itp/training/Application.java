2a121e68af9fa1edd5e9f8db35c9abc069243d70
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-09 08:54:07 +0530

    using hashmap

--- collections-fundamentals/src/com/itp/training/Application.java
+++ collections-fundamentals/src/com/itp/training/Application.java
@@ -1,20 +1,25 @@
 package com.itp.training;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		Map nameMap = new Hashtable();
+		Map nameMap = new HashMap();
 
 		nameMap.put(10, "SUNIL");
 		nameMap.put(11, "ANIL");
 		nameMap.put(12, "SHIVANSH");
 		nameMap.put(13, "GANESH");
 		nameMap.put(14, "MAULI");
-
+		nameMap.put(15,null);
+		nameMap.put(16,null);
+		nameMap.put(null,"AKASH");
+		nameMap.put(null,"AKSHAY");
+		nameMap.put(14, "MANDAL");
+		
 		System.out.println(nameMap);
 		
 		Object obj = nameMap.get(12);
@@ -23,7 +28,7 @@ public class Application {
 		System.out.println("20->"+nameMap.get(20));
 		
 		System.out.println("20->"+nameMap.getOrDefault(20, "SATISH"));
-		
+
 		
 		
 	}
