fe0e65ca75f533b0a5c4b63edaa686848cea6225
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-09 08:23:16 +0530

    set methods

--- collections-fundamentals/src/com/itp/training/Application.java
+++ collections-fundamentals/src/com/itp/training/Application.java
@@ -1,7 +1,7 @@
 package com.itp.training;
 
-import java.util.Collection;
-import java.util.LinkedList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 class A {
 	@Override
@@ -14,55 +14,21 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		Collection c = new LinkedList();
+		Set c = new LinkedHashSet();
 		c.add(10);
 		c.add("SUNIL");
 		c.add(10.5);
 		c.add(new A());
 		c.add("BLUE");
-
-		System.out.println(c);
-
-		c.remove("SUNIL");
-		System.out.println(c);
-
+		c.add("BLUE");
+		c.add("BLUE");
+		c.add("BLUE");
+		c.add("BLUE");
+		c.add("BLUE");
+		c.add("BLUE");
+		
 		System.out.println("Size:" + c.size());
-
-		Collection c1 = new LinkedList();
-		c1.add("BLUE");
-		c1.add("ORANGE");
-
-		c.addAll(c1);
 		System.out.println(c);
-
-		System.out.println("Contains:" + c.contains("BLUE"));
-
-		System.out.println("ContainsAll:" + c.containsAll(c1));
-
-		// delete all elements from collection
-//		c.clear();
-//		System.out.println("Size:"+c.size());
-
-		System.out.println("isEmpty:" + c.isEmpty());
-		// c.removeAll(c1);
-
-		System.out.println(c);
-
-		// c.removeIf(f->f)
-
-		// c.retainAll(c1);
-		System.out.println(c);
-
-		Object[] data = c.toArray();
-
-		//java 8 - forEach loop with lambda
-		c.forEach(e -> System.out.println(e));
 
 	}
 }
-
-
-
-
-
-
