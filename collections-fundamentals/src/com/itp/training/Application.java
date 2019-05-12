57554d3d4bed9ac901e14957414ae5dc42fef0d1
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-12 08:34:32 +0530

    Writing properties using props store writer

--- collections-fundamentals/src/com/itp/training/Application.java
+++ collections-fundamentals/src/com/itp/training/Application.java
@@ -1,7 +1,9 @@
 package com.itp.training;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 class Student implements Comparable<Student> {
 	private int id;
@@ -26,7 +28,7 @@ class Student implements Comparable<Student> {
 			return true;
 		if (obj != null && obj instanceof Student) {
 			Student s = (Student) obj;
-			if (this.id == s.id && this.name.equals(s.name) ) {
+			if (this.id == s.id && this.name.equals(s.name)) {
 				return true;
 			}
 		}
@@ -35,8 +37,8 @@ class Student implements Comparable<Student> {
 
 	@Override
 	public int hashCode() {
-		//prime numbers
-		int sHash =name.hashCode();
+		// prime numbers
+		int sHash = name.hashCode();
 		return id + sHash;
 	}
 
@@ -59,16 +61,26 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		Map<Student, Integer> map = new HashMap();
+		Properties props = new Properties();
+		props.setProperty("server.port", "1900");
+		props.setProperty("app.name", "movie-service");
 
-		map.put(null, 100);
-		map.put(new Student(3, "ANIL", 25), 200);
-		Integer oldValue = map.put(new Student(3, "GANESH", 23), 300);
+		try (Writer writer = 
+				Files.newBufferedWriter(Paths.get("config.properties"))) {
+			props.store(writer, "Application Settings");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-		System.out.println("Old value:"+oldValue);
-		//System.out.println(map);
-		
-		System.out.println(map.get(new Student(3, "ANIL", 25)));
-
+		/*
+		 * Map<Student, Integer> map = new HashMap();
+		 * 
+		 * map.put(null, 100); map.put(new Student(3, "ANIL", 25), 200); Integer
+		 * oldValue = map.put(new Student(3, "GANESH", 23), 300);
+		 * 
+		 * System.out.println("Old value:"+oldValue); //System.out.println(map);
+		 * 
+		 * System.out.println(map.get(new Student(3, "ANIL", 25)));
+		 */
 	}
 }
