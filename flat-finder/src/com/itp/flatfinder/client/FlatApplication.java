db9712924cae11a8c122418411b1a547709cbeca
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-15 08:14:33 +0530

    added key/value pair for new params to flat finding logic

--- flat-finder/src/com/itp/flatfinder/client/FlatApplication.java
+++ flat-finder/src/com/itp/flatfinder/client/FlatApplication.java
@@ -1,7 +1,9 @@
 package com.itp.flatfinder.client;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.itp.flatfinder.model.Flat;
 import com.itp.flatfinder.util.FlatCostCalculator;
@@ -15,10 +17,15 @@ public class FlatApplication {
 		flats.add(new Flat("B", 12000, 1, 15, 100));
 		flats.add(new Flat("C", 11000, 4, 20, 1500));
 		
-		FlatCostCalculator.calculateTotalCost(flats, 10, 5, 20);
+		Map<String,Integer> costParams = new HashMap();
+		costParams.put("totalWorkingDays",20);
+		costParams.put("distanceCost",10);
+		costParams.put("travelCost",5);
+		costParams.put("travelCost",5);
+		
+		FlatCostCalculator.calculateTotalCost(flats, costParams);
 		
 		flats.forEach(f-> {
-			
 			System.out.println(f.getName()+"\t"+f.getTotalCost());
 		});
 		
--- flat-finder/src/com/itp/flatfinder/util/FlatCostCalculator.java
+++ flat-finder/src/com/itp/flatfinder/util/FlatCostCalculator.java
@@ -1,21 +1,22 @@
 package com.itp.flatfinder.util;
 
 import java.util.List;
+import java.util.Map;
 
 import com.itp.flatfinder.model.Flat;
 
 public class FlatCostCalculator {
 
 	// Call by Value & Call by Ref in java.
-	public static void calculateTotalCost(List<Flat> flats, int distanceCost, 
-			int travelCost, int totalWorkingDays) {
-		flats.parallelStream().forEach(f->{
+	public static void calculateTotalCost(List<Flat> flats, 
+			Map<String,Integer> costParams) {
+		flats.parallelStream().forEach(f -> {
 			int totalCost = 0;
 			totalCost += f.getRentPerMonth();
-			totalCost += (f.getDistance() * distanceCost) * totalWorkingDays;
-			totalCost += (f.getTravelTime() * travelCost) * totalWorkingDays;
+			totalCost += (f.getDistance() * costParams.get("distanceCost")) * costParams.get("totalWorkingDays");
+			totalCost += (f.getTravelTime() * costParams.get("travelCost")) * costParams.get("totalWorkingDays");
 			totalCost -= f.getLocationAdvantage();
 			f.setTotalCost(totalCost);
 		});
-	} 
+	}
 }
