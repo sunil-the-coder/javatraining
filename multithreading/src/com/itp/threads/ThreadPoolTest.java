244e9beab072a33da4d3abad9ff0b76e1b1a589e
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-25 11:19:58 +0530

    single thread model using executorservice

--- multithreading/src/com/itp/threads/ThreadPoolTest.java
+++ multithreading/src/com/itp/threads/ThreadPoolTest.java
@@ -3,14 +3,31 @@ package com.itp.threads;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class Task implements Runnable {
+
+	@Override
+	public void run() {
+		for (;;) {
+			System.out.println("Working....");
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
 public class ThreadPoolTest {
 
 	public static void main(String[] args) {
+
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
 		
-		ExecutorService service = Executors.newFixedThreadPool(4);
+		executorService.submit(new Task());
 		
-		//Callable
-		//service.submit(new Deposit())
-		
+		//Close the pool & clean up all threads.
+		executorService.shutdown();
 	}
 }
