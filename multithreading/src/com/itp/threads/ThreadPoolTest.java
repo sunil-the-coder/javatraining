a40d01189ce4d2cc72d0961d43263fb9279e11ef
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-25 12:24:57 +0530

    using callable threads

--- multithreading/src/com/itp/threads/ThreadPoolTest.java
+++ multithreading/src/com/itp/threads/ThreadPoolTest.java
@@ -1,16 +1,19 @@
 package com.itp.threads;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 class Task implements Runnable {
 
 	@Override
 	public void run() {
 		for (;;) {
-			System.out.println("Working....");
+			System.out.println(Thread.currentThread().getName() + " Working....");
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -19,15 +22,42 @@ class Task implements Runnable {
 
 }
 
+class StringSplitter implements Callable<String> {
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(1000);
+		return "Dummy String";
+	}
+}
+
 public class ThreadPoolTest {
 
 	public static void main(String[] args) {
 
-		ExecutorService executorService = Executors.newSingleThreadExecutor();
-		
-		executorService.submit(new Task());
-		
-		//Close the pool & clean up all threads.
+		Runtime runtime = Runtime.getRuntime();
+
+		// Get no of processors on current machine
+		int processors = runtime.availableProcessors();
+
+		System.out.println(processors);
+
+		// Prepare service based on thread model.
+		ExecutorService executorService = Executors.newFixedThreadPool(processors * 2);
+
+		// Submit any Runnable / Callable task
+		Future<String> future = executorService.
+				submit(new StringSplitter());
+
+		try {
+			System.out.println("Waiting for result....");
+			String result = future.get();
+			System.out.println("result:"+result);
+
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		// Close the pool & clean up all threads.
 		executorService.shutdown();
 	}
 }
