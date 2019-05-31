b707822a4aaebacf7ed9687e9ad37c0e94fbc635
Sunil Patil <sunilnobeltraining@gmail.com>
2019-05-31 10:05:12 +0530

    add dynamic value to sql query

--- jdbc/src/jdbc/Test.java
+++ jdbc/src/jdbc/Test.java
@@ -11,27 +11,30 @@ public class Test {
 	public static void main(String[] args) {
 
 		try {
-			//Class.forName("com.mysql.jdbc.Driver");
+			// Class.forName("com.mysql.jdbc.Driver");
 
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nobel", "sunil", "sunil@123");
+			Connection conn = DriverManager.
+					getConnection("jdbc:mysql://localhost:3306/nobel", "sunil", "sunil@123");
 
 			Statement stmt = conn.createStatement();
 
-			ResultSet rs = stmt.executeQuery("select *from student");
-			
-			//iterate over the result
-			while(rs.next()) {
+			String sname = "anil";
+			ResultSet rs = stmt.executeQuery(
+					"select * from student  where name='"+sname+"'");
+
+			// iterate over the result
+			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString(2);
 				String phone = rs.getString("phone");
-				System.out.println(id+"\t"+name+"\t"+phone);
+				System.out.println(id + "\t" + name + "\t" + phone);
 			}
-			
+
 			rs.close();
 			stmt.close();
 			conn.close();
-			
-		}  catch (SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
