1c129ef3c9286a7666a454819de8d8343f035fe5
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-01 11:56:27 +0530

    using autocloseable feature to close resources automatically ( java.17)

--- jdbc/src/jdbc/Test.java
+++ jdbc/src/jdbc/Test.java
@@ -3,29 +3,20 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Connection conn = null;
-		PreparedStatement ps = null;
 
-		try {
-			// Class.forName("com.mysql.jdbc.Driver");
+		String deleteQuery = "delete from student where id=?";
 
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nobel", "sunil", "sunil@123");
+		try (Connection conn = DriverManager.
+				getConnection("jdbc:mysql://localhost:3306/nobel", 
+						"sunil", "sunil@123");
+				PreparedStatement ps = conn.prepareStatement(deleteQuery);) {
 
-			System.out.println(conn);
-			// "select * from student where name=? and id=?"
-			// String insertQuery = "insert into student values(?,?,?)";
-			// String updateQuery = "update student set phone=? where id=?";
-
-			String deleteQuery = "delete from student where id=?";
-
-			ps = conn.prepareStatement(deleteQuery);
-			ps.setInt(1, 2);
+			ps.setInt(1, 3);
 
 			int res = ps.executeUpdate();
 			if (res >= 1) {
@@ -34,27 +25,8 @@ public class Test {
 				System.out.println("Deletion Failed");
 			}
 
-			/*
-			 * ResultSet rs = ps.executeQuery(); // iterate over the result while
-			 * (rs.next()) { int id = rs.getInt("id"); String name = rs.getString(2); String
-			 * phone = rs.getString("phone"); System.out.println(id + "\t" + name + "\t" +
-			 * phone); }
-			 */
-			// rs.close();
-
-			
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (ps != null)
-					ps.close();
-				if (conn != null)
-					conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
 		}
 	}
 }
