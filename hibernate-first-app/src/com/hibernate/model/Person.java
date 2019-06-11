1bc173e95720e1b4752d9888f3f354fdad44c8e8
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-11 08:44:35 +0530

    joined strategy

--- hibernate-first-app/src/com/hibernate/model/Person.java
+++ hibernate-first-app/src/com/hibernate/model/Person.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 @Table(name = "person")
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Person {
 
 	@Id
