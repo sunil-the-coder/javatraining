5226d95d4d6590d860748988349169d320cee2d0
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-13 07:24:19 +0530

    critera api

--- hibernate-first-app/src/com/hibernate/HibernateTest.java
+++ hibernate-first-app/src/com/hibernate/HibernateTest.java
@@ -1,37 +1,32 @@
 package com.hibernate;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import com.hibernate.model.Person;
 
 public class HibernateTest {
-	
+
 	public static void main(String[] args) {
-		
+
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
 		Session session = sessionFactory.openSession();
-		
-		Person p = (Person)session.get(Person.class, 1);
-		System.out.println(p);
-		
-		p = (Person)session.get(Person.class, 1);
-		System.out.println(p);
-	
-		p = (Person)session.get(Person.class, 1);
-		System.out.println(p);
-		
+
+		// select * from person
+		Criteria criteria = session.createCriteria(Person.class);
+
+		List<Person> persons = (List<Person>) criteria.list();
+
+		persons.forEach(p->System.out.println(p));
+		// Person p = (Person) session.get(Person.class, 1);
+		// System.out.println(p);
+
 		session.close();
 
-		
-		Session session2 = sessionFactory.openSession();
-		
-		p = (Person)session2.get(Person.class, 1);
-		System.out.println(p);
-		
-		session2.close();
-		
 		sessionFactory.close();
 	}
 
