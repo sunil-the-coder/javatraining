dc730a0903826710720225ee00855cf76c188b82
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-20 07:55:39 +0530

    servlet introduction with login app

---
+++ login-app/src/com/itp/servlets/AuthenticateUser.java
@@ -0,0 +1,39 @@
+package com.itp.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/authenticate")
+public class AuthenticateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		
+		//out.println("Authenticating User.....");
+		
+		String username = request.getParameter("uname");
+		String password = request.getParameter("pwd");
+		
+		if(username.equals("sunil") && password.equals("patil")) {
+			out.println("Login Success.");
+		}else {
+			//response.sendRedirect("index.html");
+			
+			RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+			dispatcher.forward(request, response);
+		}
+		
+		out.close();
+	}
+
+}
