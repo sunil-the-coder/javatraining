70a2a6ed5b6a9330d1b6410dcee3588283c4f2b7
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-21 13:11:51 +0530

    validate user from database

--- login-app/src/com/itp/servlets/AuthenticateUser.java
+++ login-app/src/com/itp/servlets/AuthenticateUser.java
@@ -2,6 +2,11 @@ package com.itp.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -26,17 +31,46 @@ public class AuthenticateUser extends HttpServlet {
 		String password = request.getParameter("pwd");
 
 		// DB Connection validation
+		
+		
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			//DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
+		String validationQuery = "select * from users where uname=? and passwd=?";
+		try (Connection conn = DriverManager.
+				getConnection("jdbc:mysql://localhost:3306/nobel", 
+						"sunil", "sunil@123");
+				PreparedStatement ps = conn.prepareStatement(validationQuery);
+				) {
 
-		if (username.equals("sunil") && password.equals("patil")) {
-			// out.println("Login Success.");
-			// response.sendRedirect("categories");
-		} else {
-			// response.sendRedirect("index.html");
+			ps.setString(1, username);
+			ps.setString(2, password);
 
-			RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
-			dispatcher.forward(request, response);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				response.sendRedirect("categories");
+			} else {
+				response.sendRedirect("index.html");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 
+
+		/*
+		 * if (username.equals("sunil") && password.equals("patil")) { //
+		 * out.println("Login Success."); // response.sendRedirect("categories"); } else
+		 * { // response.sendRedirect("index.html");
+		 * 
+		 * RequestDispatcher dispatcher = request.getRequestDispatcher("index.html");
+		 * dispatcher.forward(request, response); }
+		 */
 		out.close();
 
 	}
