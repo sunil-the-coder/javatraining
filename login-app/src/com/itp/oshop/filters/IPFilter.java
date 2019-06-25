0623a060b1c5cd1ec43b7011dc2e421ce2dfa758
Sunil Patil <sunilnobeltraining@gmail.com>
2019-06-25 08:55:36 +0530

    filter using extension mapping

--- login-app/src/com/itp/oshop/filters/IPFilter.java
+++ login-app/src/com/itp/oshop/filters/IPFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-@WebFilter(urlPatterns = { "/admin/*" })
+@WebFilter(urlPatterns = { "*.secured" })
 public class IPFilter implements Filter {
 
 	@Override
