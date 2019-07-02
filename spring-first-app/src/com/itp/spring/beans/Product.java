2e9319ca0b225301137f55305a7dacbe57f43be4
Sunil Patil <sunilnobeltraining@gmail.com>
2019-07-02 08:36:25 +0530

    first spring app with product data injection

---
+++ spring-first-app/src/com/itp/spring/beans/Product.java
@@ -0,0 +1,41 @@
+package com.itp.spring.beans;
+
+public class Product {
+
+	private int id = 10;
+	private String name = "adidas shoes";
+	//private Rating rating;
+
+	public Product() {
+		System.out.println("def ctr called");
+	}
+	public Product(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		System.out.println("setting id:"+id);
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println("setting name:"+name);
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + "]";
+	}
+
+}
