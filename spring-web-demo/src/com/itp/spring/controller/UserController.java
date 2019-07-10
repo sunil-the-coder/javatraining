1059d53d3fc1e3636ef12cdbee3c9c5ca5cefdca
Sunil Patil <sunilnobeltraining@gmail.com>
2019-07-10 08:34:46 +0530

    integrat hibernate with spring

--- spring-web-demo/src/com/itp/spring/controller/UserController.java
+++ spring-web-demo/src/com/itp/spring/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.itp.spring.controller;
 
+import javax.servlet.ServletContext;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +17,9 @@ import com.itp.spring.model.User;
 @Controller
 public class UserController {
 
+	@Autowired
+	private ServletContext context;
+
 	@RequestMapping("/prepareUser")
 	public ModelAndView populateUser() {
 		ModelAndView modelAndView = new ModelAndView();
@@ -21,8 +30,21 @@ public class UserController {
 
 	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
 	public ModelAndView addUser(@ModelAttribute User user) {
-		
+
+		SessionFactory sessionFactory = (SessionFactory) context.getAttribute("sessionFactory");
+
+		Session session = sessionFactory.openSession();
+
+		Transaction txn = session.beginTransaction();
+
+		session.save(user);
+
+		txn.commit();
+
+		session.close();
+
 		System.out.println(user);
+
 		return new ModelAndView("success", "msg", "User Added");
 	}
 
--- spring-web-demo/src/com/itp/spring/model/User.java
+++ spring-web-demo/src/com/itp/spring/model/User.java
@@ -1,7 +1,19 @@
 package com.itp.spring.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
 	private String uname;
 	private String pwd;
 
@@ -26,5 +38,4 @@ public class User {
 		return "User [uname=" + uname + ", pwd=" + pwd + "]";
 	}
 
-	
 }
