fb2fa2aff6c8db7d862e4abe68a37dea1e1726a8
Sunil Patil <sunilnobeltraining@gmail.com>
2019-07-20 08:54:03 +0530

    lambda expressions

--- java8-features/src/Test.java
+++ java8-features/src/Test.java
@@ -10,47 +10,43 @@ interface A {
 
 class B {
 	public void show(A a) {
-		a.disp(10,20);
+		a.disp(10, 20);
 	}
 }
 
-
-/*class OddThread implements Runnable
-{
-	@Override
-	public void run() {
-		for(;;) {
-			System.out.println("Odd thread running....");
-		}
-	}
-}*/
+/*
+ * class OddThread implements Runnable {
+ * 
+ * @Override public void run() { for(;;) {
+ * System.out.println("Odd thread running...."); } } }
+ */
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Runnable runnable = new Runnable() {
-			@Override
-			public void run() {
-				for(;;) {
-					System.out.println("Odd thread running....");
-				}		
+
+		/*
+		 * Runnable runnable = new Runnable() {
+		 * 
+		 * @Override public void run() { for(;;) {
+		 * System.out.println("Odd thread running...."); } } };
+		 */
+
+		Runnable runnable = () -> {
+			for (;;) {
+				System.out.println("Odd thread running....");
 			}
 		};
 		
 		Thread t1 = new Thread(runnable);
-		
+
 		t1.start();
-		
-		
-		
-		
+
 		B b = new B();
-		A a = (p,q) -> System.out.println("after call for p...."+p);
-		A a1 = (q,s) -> System.out.println("after call for q...."+q);
+		A a = (p, q) -> System.out.println("after call for p...." + p);
+		A a1 = (q, s) -> System.out.println("after call for q...." + q);
 		b.show(a);
-		
-		
+
 		/*
 		 * new Thread(()-> { for(;;) { System.out.println("Thread rann"); try {
 		 * TimeUnit.MILLISECONDS.sleep(500); } catch (InterruptedException e) {
