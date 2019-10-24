4dc09f0f7f17676b67e407b8065a51e62961dda3
Sunil Patil <sunilnobeltraining@gmail.com>
2019-10-24 09:42:03 +0530

    bypassing exception handling to caller

--- pbatch10/Examples/src/com/spatil/exception/TestException.java
+++ pbatch10/Examples/src/com/spatil/exception/TestException.java
@@ -1,50 +1,26 @@
 package com.spatil.exception;
 
+import java.io.IOException;
+
 public class TestException {
 
 	public static void main(String[] args) {
-
-		System.out.println("1");
-		System.out.println("2");
-
+		
 		try {
-
-			int v = Integer.parseInt("1o");
-			System.out.println(v);
-
-			int ans = 10 / 0;
-			System.out.println("Ans is:" + ans);
-
-		} catch (ArithmeticException e) {
-			System.out.println("Can't divide by zero");
-		} catch(NumberFormatException e) {
-			System.out.println("Invalid number provided. Please provide numeric value.");
+			f1();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-
-		// f1();
-
-		System.out.println("3");
-		System.out.println("5");
-		System.out.println("6");
+		
 	}
 
-	private static void f1() {
+	private static void f1() throws IOException {
+
 		f2();
+		
 	}
 
-	private static void f2() {
-
-		try {
-
-			int ans = 10 / 0;
-			System.out.println("Ans is:" + ans);
-
-		} catch (ArithmeticException e) {
-			// e.printStackTrace();
-			System.out.println(e.getMessage());
-
-			// System.out.println("Please provide non-zero numerator value. Can't divide by
-			// zero");
-		}
+	private static void f2() throws IOException  {
+		System.in.read();
 	}
 }
