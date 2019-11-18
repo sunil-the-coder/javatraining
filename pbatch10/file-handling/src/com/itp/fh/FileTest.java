a06e45ffe34d72144ddbbd28f1c0a3214daa766d
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-18 09:44:32 +0530

    working with file object for directory listing

--- pbatch10/file-handling/src/com/itp/fh/FileTest.java
+++ pbatch10/file-handling/src/com/itp/fh/FileTest.java
@@ -7,15 +7,43 @@ public class FileTest {
 	public static void main(String[] args) {
 
 		
-		File file = new File("/home/sunil/lines.txt");
+		File file = new File("/home/sunil/javatraining");
+		
+		String[] dirContents = file.list();
+		
+		for (String name : dirContents) {
+			System.out.println(name);
+		}
+		
+		
+		File[] files = file.listFiles();
+		
+		for(File f : files) {
+			System.out.println(f.getAbsolutePath());
+			
+			if(f.canExecute()) {
+				//do somethingg....
+				
+			}
+			
+			if(f.getName().endsWith(".txt")) {
+				//delete all files
+				
+			}
+			
+			
+		}
 		
 		//File file = new File("D:\\javaprogram\\lines.txt"); -> Windows
 		
-		System.out.println(file.canRead());
-		System.out.println(file.length() +" bytes");
-		System.out.println(file.exists());
-		System.out.println(file.getParent());
-		System.out.println(file.isFile());
-		System.out.println(file.isHidden());
+		/*
+		 * System.out.println(file.canRead()); System.out.println(file.length()
+		 * +" bytes"); System.out.println(file.exists());
+		 * System.out.println(file.getParent()); System.out.println(file.isFile());
+		 * System.out.println(file.isHidden());
+		 */
+		
+		
+		
 	}
 }
