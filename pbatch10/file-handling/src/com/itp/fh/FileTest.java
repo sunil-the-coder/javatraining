2a640f4594d2b7877f5bc08e5c34df9607bd3a72
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-20 08:45:54 +0530

    serialization for custom java object

--- pbatch10/file-handling/src/com/itp/fh/FileTest.java
+++ pbatch10/file-handling/src/com/itp/fh/FileTest.java
@@ -2,13 +2,32 @@ package com.itp.fh;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class Student implements Serializable {
+	private int id;
+	private String name;
+
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return " [id=" + id + ", name=" + name + "]";
+	}
+
+}
 
 public class FileTest {
 
 	public static void copyFile(String srcFile, String destFile) {
 
-		try (FileInputStream fr = new FileInputStream(srcFile); 
-				FileOutputStream fw = new FileOutputStream(destFile);) {
+		try (FileInputStream fr = new FileInputStream(srcFile); FileOutputStream fw = new FileOutputStream(destFile);) {
 
 			int v = -1;
 			while ((v = fr.read()) != -1) {
@@ -24,53 +43,34 @@ public class FileTest {
 	}
 
 	public static void main(String[] args) {
+		// copyFile("/home/sunil/demo.png", "/home/sunil/demo2.png");
 
-		copyFile("/home/sunil/demo.png", "/home/sunil/demo2.png");
+		// Write custom java object to file - Serialization
+		try (FileOutputStream fos = new FileOutputStream("/home/sunil/student.ser");
+				ObjectOutputStream oos = new ObjectOutputStream(fos);) {
 
-//		FileReader fr = null;
-//
-//		try {
-//
-//			// Resource
-//			fr = new FileReader("/home/sunil/lines.txt");
-//
-//			int v = -1;
-//			while ((v = fr.read()) != -1) {
-//				System.out.print((char) v);
-//			}
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//		// Close all resources in finally block
-//		finally {
-//			try {
-//				if (fr != null)
-//					fr.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//
-//		FileWriter fw = null;
-//
-//		try {
-//
-//			fw = new FileWriter("/home/sunil/lines1.txt");
-//			fw.write("this is sunil from java program");
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} finally {
-//			try {
-//				if (fw != null)
-//					fw.close();
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//
-//		}
+			Student stud = new Student(10, "Sunil");
+			oos.writeObject(stud);
+
+			System.out.println("Student object stored on file..");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// read custom java object from file - Deserialization
+		try (FileInputStream fis = new FileInputStream("/home/sunil/student.ser");
+				ObjectInputStream ois = new ObjectInputStream(fis);) {
+
+			System.out.println("Reading student object back from file...");
+			
+			Object obj = ois.readObject();
+			Student stud = (Student) obj;
+			System.out.println(stud);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
+
 }
