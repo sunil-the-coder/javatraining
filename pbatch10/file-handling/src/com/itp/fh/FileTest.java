dd4a1ab97a7cf1e0de2a9d4083c24333e2a16774
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-20 08:12:28 +0530

    copy file

--- pbatch10/file-handling/src/com/itp/fh/FileTest.java
+++ pbatch10/file-handling/src/com/itp/fh/FileTest.java
@@ -1,57 +1,95 @@
 package com.itp.fh;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileTest {
 
-	public static void main(String[] args) {
+	public static void copyFile(String srcFile, String destFile) {
+
 		FileReader fr = null;
+		FileWriter fw = null;
 
 		try {
-
-			// Resource
-			fr = new FileReader("/home/sunil/lines.txt");
+			fr = new FileReader(srcFile);
+			fw = new FileWriter(destFile);
 
 			int v = -1;
 			while ((v = fr.read()) != -1) {
-				System.out.print((char) v);
+				//System.out.print((char)v);
+				fw.write(v);
 			}
-
-		} catch (IOException e) {
+			
+			System.out.println("File is copied.");
+			
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
-		}
-
-		// Close all resources in finally block
-		finally {
-			try {
-				if (fr != null)
-					fr.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-
-		FileWriter fw = null;
-		
-		try {
-
-			fw = new FileWriter("/home/sunil/lines1.txt");
-			fw.write("this is sunil from java program");
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
+				if (fr != null)
+					fr.close();
 				if (fw != null)
 					fw.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
 		}
+
+	}
+
+	public static void main(String[] args) {
+		
+		copyFile("/home/sunil/Demo.cpp", "/home/sunil/Demo2.cpp");
+		
+		
+//		FileReader fr = null;
+//
+//		try {
+//
+//			// Resource
+//			fr = new FileReader("/home/sunil/lines.txt");
+//
+//			int v = -1;
+//			while ((v = fr.read()) != -1) {
+//				System.out.print((char) v);
+//			}
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//
+//		// Close all resources in finally block
+//		finally {
+//			try {
+//				if (fr != null)
+//					fr.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//
+//		FileWriter fw = null;
+//
+//		try {
+//
+//			fw = new FileWriter("/home/sunil/lines1.txt");
+//			fw.write("this is sunil from java program");
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} finally {
+//			try {
+//				if (fw != null)
+//					fw.close();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//
+//		}
 
 	}
 }
