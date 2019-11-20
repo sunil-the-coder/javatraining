90a2d787215839f982d1a670a200f48e2e4acd7a
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-20 08:05:00 +0530

    closing resources using inally block

--- pbatch10/file-handling/src/com/itp/fh/FileTest.java
+++ pbatch10/file-handling/src/com/itp/fh/FileTest.java
@@ -7,30 +7,50 @@ import java.io.IOException;
 public class FileTest {
 
 	public static void main(String[] args) {
+		FileReader fr = null;
 
 		try {
 
-			FileReader fr = new FileReader("/home/sunil/lines.txt");
+			// Resource
+			fr = new FileReader("/home/sunil/lines.txt");
 
 			int v = -1;
 			while ((v = fr.read()) != -1) {
 				System.out.print((char) v);
 			}
 
-			fr.close();
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		// Close all resources in finally block
+		finally {
+			try {
+				if (fr != null)
+					fr.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+
+		FileWriter fw = null;
+		
 		try {
 
-			FileWriter fw = new FileWriter("/home/sunil/lines1.txt");
+			fw = new FileWriter("/home/sunil/lines1.txt");
 			fw.write("this is sunil from java program");
-			fw.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (fw != null)
+					fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
