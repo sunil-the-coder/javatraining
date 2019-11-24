928f24b7c10260b920d3028218239110b62ae372
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-24 11:24:11 +0530

    2.method local inner class

--- pbatch10/nested-classes/src/NestedTest.java
+++ pbatch10/nested-classes/src/NestedTest.java
@@ -2,39 +2,27 @@ class Outer {
 
 	private int data = 10;
 
-	
-	static int mark = 100;
-	
-	
 	public void disp() {
 		System.out.println("Outer disp called " + data);
-	}
 
-	static class Inner {
-		public void show() {
-			System.out.println("Inner show called..");
+		//Method local inner class
+		class Inner {
+			public void show() {
+				System.out.println("Inner show called..");
+			}
 		}
+		
+		Inner obj = new Inner();
+		obj.show();
 	}
-
 }
 
 public class NestedTest {
 
 	public static void main(String[] args) {
 
-		
-		Outer.mark = 200;
-		
-		//2. Creating object of static nested class directly without outer class obj
-		Outer.Inner inn = new Outer.Inner();
-		inn.show();
-		
-		
-		
-		
-		
-		
-		
+		Outer outer = new Outer();
+		outer.disp();
 	}
 
 }
