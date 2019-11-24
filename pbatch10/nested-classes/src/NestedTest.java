1cb99f47c8147f09a4b383d9be69e015691a5803
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-24 11:34:38 +0530

    3. Anonymous classes

--- pbatch10/nested-classes/src/NestedTest.java
+++ pbatch10/nested-classes/src/NestedTest.java
@@ -1,3 +1,11 @@
+
+class A {
+
+}
+
+class B {
+}
+
 class Outer {
 
 	private int data = 10;
@@ -5,24 +13,66 @@ class Outer {
 	public void disp() {
 		System.out.println("Outer disp called " + data);
 
-		//Method local inner class
+		// Method local inner class
 		class Inner {
 			public void show() {
 				System.out.println("Inner show called..");
 			}
 		}
-		
+
 		Inner obj = new Inner();
 		obj.show();
 	}
 }
 
+class Demo {
+	public void show() {
+		System.out.println("Demo show called..");
+	}
+}
+
+//class NewDemo extends Demo
+//{
+//	@Override
+//	public void show() {
+//		System.out.println("New Demo show called...");
+//	}
+//}
+
+//class NestedTest$1 extends Demo
+//{
+//	@Override
+//	public void show() {
+//		System.out.println("New Version of demo show called..");
+//	}
+//}
+
+
 public class NestedTest {
 
 	public static void main(String[] args) {
 
-		Outer outer = new Outer();
-		outer.disp();
+		Demo d = new Demo() {
+			@Override
+			public void show() {
+				System.out.println("New Version of demo show called..");
+			}
+		};
+		
+		d.show();
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
