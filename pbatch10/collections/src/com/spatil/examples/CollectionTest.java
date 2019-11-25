c33c89ad740b6c16906875f859790f8bf13c67d4
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-25 08:33:08 +0530

    traverse collection

--- pbatch10/collections/src/com/spatil/examples/CollectionTest.java
+++ pbatch10/collections/src/com/spatil/examples/CollectionTest.java
@@ -1,9 +1,8 @@
 package com.spatil.examples;
 
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 class Student {
 
@@ -14,61 +13,24 @@ public class CollectionTest {
 	public static void main(String[] args) {
 
 		List list = new LinkedList();
-
 		list.add(10);
 		list.add(10.5);
 		list.add("sunil");
-		//list.add(new Student());
-		
-		System.out.println(list);
-		
-		list.remove("sunil");
-		
-		System.out.println(list);
-		
-		list.add(2, "Ganesh");
-		System.out.println(list);
-		
-		System.out.println(list.contains(10));
-		
-		//list.clear();  //delete all elements from collection
-		//System.out.println(list);
 
-		Object element = list.get(2);
-		System.out.println(element);
-		
-		
-		System.out.println(list.indexOf("Satish"));
-		
-		List newList = list.subList(0, 2);
-		System.out.println(newList);
-		
-		Object[]  arr = list.toArray();
-		
-		
-		List list2 = new LinkedList();
-		list2.add("anil");
-		list2.add("raju");
-		list2.add("arvind");
-		
-		
-		Set set = new HashSet();
-		set.add(100);
-		set.add(200);
-		
-		
-		//add one collection into another collection
-		list.addAll(list2);
-		
-		list.add(set);
-		
-		
 		System.out.println(list);
-
-		list.removeAll(list2);
-		System.out.println(list);
-
+		
+		for(int i = 0; i < list.size(); i++)
+			System.out.println(list.get(i));
 		
 		
+		for(Object obj : list)
+			System.out.println(obj);
+		
+		System.out.println("********** Using iterator *************");
+		Iterator itr = list.iterator();
+		while(itr.hasNext()) {
+			Object element = itr.next();
+			System.out.println(element);
+		}
 	}
 }
