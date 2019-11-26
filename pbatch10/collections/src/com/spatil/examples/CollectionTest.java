d0205629ea7bb3000e77cf87eaaa7a8bf42f650e
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-26 08:46:21 +0530

    using maps with generics

--- pbatch10/collections/src/com/spatil/examples/CollectionTest.java
+++ pbatch10/collections/src/com/spatil/examples/CollectionTest.java
@@ -1,22 +1,19 @@
 package com.spatil.examples;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CollectionTest {
 
 	public static void main(String[] args) {
 
-		Set<Integer> set =  new LinkedHashSet<>();
-		set.add(50);
-		set.add(10);
-		set.add(15);
-		set.add(5);
-		set.add(50);
-		set.add(100);
-		set.add(5);
+		Map<Integer,String> map = new LinkedHashMap<>();
+		map.put(10, "Sunil");
+		map.put(14, "Akshay");
+		map.put(12, "Sandip");
+		map.put(11, "Ganesh");
+		map.put(13, "Anil");
 		
-		System.out.println(set);
-
+		System.out.println(map);
 	}
 }
