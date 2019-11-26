8621cc98a5f29213756b072d652736c909f2c94d
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-26 08:14:55 +0530

    user defined generic classes

--- pbatch10/collections/src/com/spatil/examples/CollectionTest.java
+++ pbatch10/collections/src/com/spatil/examples/CollectionTest.java
@@ -3,7 +3,18 @@ package com.spatil.examples;
 import java.util.ArrayList;
 import java.util.List;
 
-class Student {
+//User defined generic class
+class Student<T> {
+	T id;
+
+	public Student(T id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + "]";
+	}
 
 }
 
@@ -11,30 +22,32 @@ public class CollectionTest {
 
 	public static int getSum(List<Integer> list) {
 		int total = 0;
-		
-		for(Integer element : list) {
+
+		for (Integer element : list) {
 			total = total + element;
 		}
-		
+
 		return total;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		List<Integer> list = new ArrayList<>();
 		list.add(10);
 		list.add(20);
 		list.add(30);
-		
+
 		int total = getSum(list);
+
+		System.out.println("Total is:" + total);
+
 		
-		System.out.println("Total is:"+total);
+		Student<Integer> stud = new Student<>(10);
+		System.out.println(stud);
+		
+		Student<String> stud2 = new Student<>("52525");
+		System.out.println(stud2);
+		
 		
 	}
 }
-
-
-
-
-
-
