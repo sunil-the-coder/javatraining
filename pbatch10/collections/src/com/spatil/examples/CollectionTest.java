e9f91230caa79eccacf247078df982895167225b
Sunil Patil <sunilnobeltraining@gmail.com>
2019-11-27 08:48:55 +0530

    CRUD operation with list

--- pbatch10/collections/src/com/spatil/examples/CollectionTest.java
+++ pbatch10/collections/src/com/spatil/examples/CollectionTest.java
@@ -1,6 +1,7 @@
 package com.spatil.examples;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 class Book {
@@ -13,6 +14,10 @@ class Book {
 		this.id = id;
 		this.name = name;
 		this.author = author;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 	public String getName() {
@@ -57,21 +62,38 @@ public class CollectionTest {
 		books.add(new Book(11, "SCJP", "Kathy Sierra"));
 		books.add(new Book(12, "Head First Java", "Kathy Sierra"));
 		books.add(new Book(13, "Advanced Programming with Java", "Kathy Sierra"));
+		books.add(new Book(14, "Core Java", "Scott"));
 
+		System.out.println("\n************ All Books **************");
+		for (Book book : books) {
+			System.out.println(book);
+		}
+		
+		Iterator<Book> itr = books.iterator();
+		while (itr.hasNext()) {
+			Book book = itr.next();
+			if (book.getId() == 14)
+				itr.remove(); // Safe way to delete element from collection while iterating over it.
+		}
+
+		System.out.println("\n************ After Deletion of Book by ID **************");
+		for (Book book : books) {
+			System.out.println(book);
+		}
+		
 		for (Book book : books) {
 			if (book.getName().equals("SCJP"))
 				book.setAuthor(book.getAuthor() + " & James");
 		}
 
-		System.out.println("---------- Updated Books --------------");
+		System.out.println("\n************ After updating books author name **************");	
 		for (Book book : books) {
 			System.out.println(book);
 		}
 
-		
 		List<Book> booksByAuthor = filterBooksByAuthor(books, "Scott");
 
-		System.out.println("****** Books by Author *******");
+		System.out.println("\n********* After Books by Author *******");
 		for (Book book : booksByAuthor) {
 			System.out.println(book);
 		}
