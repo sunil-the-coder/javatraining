ccc0d3c0c9be9e3870f8f53b3e7b4af78b39207f
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-05 07:53:55 +0530

    thread basic object

--- pbatch10/multithreading/src/com/itp/threads/ThreadTest.java
+++ pbatch10/multithreading/src/com/itp/threads/ThreadTest.java
@@ -1,10 +1,11 @@
 package com.itp.threads;
 
 class OddThread extends Thread {
-	
+
 	@Override
 	public void run() {
 		// Thread logic
+		System.out.println(Thread.currentThread());
 		for (int i = 1; i <= 100; i += 2)
 			System.out.println("Odd:" + i);
 	}
@@ -14,6 +15,7 @@ class EvenThread implements Runnable {
 	@Override
 	public void run() {
 		// Thread logic
+		System.out.println(Thread.currentThread());
 		for (int i = 2; i <= 100; i += 2)
 			System.out.println("Even:" + i);
 	}
@@ -23,18 +25,25 @@ public class ThreadTest {
 
 	public static void main(String[] args) {
 
+		System.out.println("Inside main()");
+		
+		Thread currentThread = Thread.currentThread();
+		
+		System.out.println(currentThread);
+		
+		
 		// 1. Odd Thread
 		OddThread oddThread = new OddThread();
-		oddThread.start(); //It will run() method
-		
-		//2. Even Thread
+		oddThread.setName("oddThread");
+		oddThread.start(); // It will run() method
+
+		// 2. Even Thread
 		EvenThread evenThread = new EvenThread();
 		Thread t = new Thread(evenThread);
+		t.setName("evenThread");
 		t.start();
 		
-		
-		/*
-		 * printOddNumber();
-		 * printEvenNumber();
-		 */ }
+		System.out.println("Exiting from main");
+	}
+
 }
