df9f519b9dfff7708ec5eccaf696bf6ce96ef2ef
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-05 08:06:41 +0530

    using join method

---
+++ pbatch10/threads/src/threads/ThreadTest.java
@@ -0,0 +1,38 @@
+package threads;
+
+class MyThread implements Runnable {
+
+	public void run() {
+		int i = 1;
+		for (; i <= 10; i+=2) {
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+}
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("in main");
+
+		System.out.println(Thread.currentThread());
+
+		Thread t = new Thread(new MyThread());
+		t.start();
+
+		
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("exit from main");
+	}
+}
