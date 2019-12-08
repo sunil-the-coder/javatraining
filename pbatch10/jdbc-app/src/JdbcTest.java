f414e502d160c8f32a991b3a32cb57419f1a8d73
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-08 15:17:10 +0530

    JDBC first app - Retrieve student data from db

---
+++ pbatch10/jdbc-app/src/JdbcTest.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcTest {
+
+	public static void main(String[] args) {
+
+		// Steps to retrieve data from db.
+
+		try {
+			// 1. Load driver into memory & drive itself registers with DriverManager.
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// 2. Obtain the database connection.
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/nobel", "sunil", "sunil@123");
+
+			// 3. Prepare the statement
+			PreparedStatement ps = conn.prepareStatement("select * from student");
+
+			// 4. Execute Query
+			ResultSet rs = ps.executeQuery();
+
+			// 5.Read row by row data from rs & get column values.
+			while (rs.next()) {
+				int roll = rs.getInt(1);
+				String name = rs.getString("name");
+				String phone = rs.getString("phone");
+
+				System.out.println(roll + "\t" + name + "\t" + phone);
+			}
+
+			// 6. Close the resources
+			rs.close();
+			ps.close();
+			conn.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ pbatch10/shubham-test-project/src/FlatTest.java
@@ -0,0 +1,14 @@
+import com.nobel.ff.model.Flat;
+import com.nobel.ff.service.FlatCostCalculatorService;
+
+public class FlatTest {
+
+	public static void main(String[] args) {
+
+		Flat flat = new Flat("A", 2000, 10, 40, 500);
+		
+		
+	//	FlatCostCalculatorService service = new FlatCostCalculatorService(flats);
+		
+	}
+}
