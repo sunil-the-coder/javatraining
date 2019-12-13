3683f24a7790a8b7e1e04e9ace8a475edcda5c24
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-13 08:43:49 +0530

    moving sessionFactory object creation to HIbernateUtil class

--- pbatch10/first-hibernate-app/src/com/nobel/hibernate/HibernateTest.java
+++ pbatch10/first-hibernate-app/src/com/nobel/hibernate/HibernateTest.java
@@ -6,40 +6,29 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.nobel.hibernate.model.Student;
+import com.nobel.hibernate.util.HibernateUtil;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
 
-		//Step by Step loading of hibernate
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();		
 		
-		//1. Load the configuration file.
-		
-		Configuration config = new Configuration();
-		config.configure("hibernate.cfg.xml");
-		
-		//2. Create sessionFactory object
-		SessionFactory sessionFactory = config.buildSessionFactory();
-		
-		//3. Open new session for any kind of operation with database.
 		Session session = sessionFactory.openSession();
-		
-		//4. Start the transaction
+
 		Transaction txn = session.beginTransaction();
-		
-		//4. Perform Your operation
-		Student stud = new Student(1, "Sunil", "82962896");
-		
+
+		Student stud = new Student(2, "Anil", "82962896");
+
 		session.save(stud);
-		
+
 		txn.commit();
-		
+
 		System.out.println("Student Object is Saved.");
-		
-		//5. Close the session & sessionFactory objects.
+
 		session.close();
+
 		
-		//Most Imp Step.
-		sessionFactory.close();
+		HibernateUtil.shutdown();
 	}
 }
---
+++ pbatch10/first-hibernate-app/src/com/nobel/hibernate/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package com.nobel.hibernate.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+
+		// 1. Load the configuration file.
+
+		Configuration config = new Configuration();
+		config.configure("hibernate.cfg.xml");
+
+		// 2. Create sessionFactory object
+		SessionFactory sessionFactory = config.buildSessionFactory();
+
+		return sessionFactory;
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		if (sessionFactory != null)
+			sessionFactory.close();
+	}
+}
