07949f39b4a4c2c123e6fe379eb89f18f0242c74
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-14 11:08:00 +0530

    workign with hql queries

--- pbatch10/first-hibernate-app/src/com/nobel/hibernate/HibernateTest.java
+++ pbatch10/first-hibernate-app/src/com/nobel/hibernate/HibernateTest.java
@@ -1,8 +1,10 @@
 package com.nobel.hibernate;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import com.nobel.hibernate.model.Student;
 import com.nobel.hibernate.util.HibernateUtil;
@@ -11,23 +13,24 @@ public class HibernateTest {
 
 	public static void main(String[] args) {
 
-		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();		
-		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
 		Session session = sessionFactory.openSession();
 
-		Transaction txn = session.beginTransaction();
+		//select * from student
+		//Query query = session.createQuery("from Student");
 		
-		Student stud = (Student) session.get(Student.class, 1);
+		Query query = session.createQuery("select OBJECT(s) from Student s");
 		
-		System.out.println(stud);
-
-		session.delete(stud);
-
-		txn.commit();
-
+		List<Student> students = (List<Student>) query.list();
+		for(Student stud : students)
+			System.out.println(stud);
+		
+		
+		
+		
 		session.close();
 
-		
 		HibernateUtil.shutdown();
 	}
 }
