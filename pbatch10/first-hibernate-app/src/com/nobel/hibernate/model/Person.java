28ea957fb7a1dc62f2fe7b940622ea363c8b27ae
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-18 16:34:11 +0530

    single table strategy

--- pbatch10/first-hibernate-app/src/com/nobel/hibernate/model/Person.java
+++ pbatch10/first-hibernate-app/src/com/nobel/hibernate/model/Person.java
@@ -5,10 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "person")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Person {
 
 	@Id
