82d03abbc1ed9c71853d7af193b09e45e498cd1a
Sunil Patil <sunilnobeltraining@gmail.com>
2019-12-27 08:46:22 +0530

    annotation based mapping

---
+++ pbatch10/hello-servlet/src/com/nobel/servlets/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.nobel.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().println("welcome to servlet 3");
+	}
+
+}
---
+++ pbatch10/hello-servlet/src/com/nobel/servlets/TestServlet.java
@@ -0,0 +1,23 @@
+package com.nobel.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServet
+ */
+//@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().print("Welcome to test servlet");
+	}
+
+}
